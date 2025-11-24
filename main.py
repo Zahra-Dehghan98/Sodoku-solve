ff1bf30ed1065d7a9634d4c2d3cf9f43d78a560a
Zahra-Dehghan98 <zahradn9877@gmail.com>
2025-11-24 16:14:20 +0330

    complete main executable with sudoku solver function using backtracking algorithm and grid display for solved - unsolved puzzles

--- main.py
+++ main.py
@@ -11,5 +11,37 @@ def import_data():
                 numbers.append(num)
             grid.append(numbers)
         return grid
+def sodoku_solve(grid):  
+    sodoku = Sodoku(grid)
+    for row in range(9):
+        for col in range(9):
+            if grid[row][col] == 0:
+                for number in range (1, 10):
+                    if sodoku.is_valid(row, col, number):
+                        grid[row][col] = number
 
-        
+                        if sodoku_solve(grid):
+                            return True
+                        grid [row][col] = 0
+                return False
+    return True
+
+def print_grid(grid):
+    for row in grid:
+        for number in row:
+            print(number, end=" ")
+        print()
+
+grid = import_data()
+print ("unsolved sodoku")
+print_grid(grid)
+print ("===================")
+if sodoku_solve(grid):
+    print("solved sodoku")
+    print_grid(grid)
+    print ("===================")
+else:
+    print("No solution exists!")
+
+
+
