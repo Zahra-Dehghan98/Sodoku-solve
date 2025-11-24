91059c736837c86f2caddd3974702a8951b8da28
Zahra Dehghan <zahradn9877@gmail.com>
2025-11-24 16:28:32 +0330

    add comprehensive docstrings to Sudoku class and all validation methods

--- sodoku.py
+++ sodoku.py
@@ -1,20 +1,25 @@
 class Sodoku():
+        """Sudoku validator class with grid validation methods"""
         def __init__(self, grid):
-              self.grid = grid
+            """Initialize with sudoku grid"""
+            self.grid = grid
 
         def check_row(self, row, number):
+            """Check if number can be placed in specified row"""
             for col in range(9):
                 if self.grid[row][col] == number:
                     return False
             return True
             
         def check_column(self, col, number):
+            """Check if number can be placed in specified column"""
             for row in range(9):
                 if self.grid[row][col] == number:
                     return False
             return True
             
         def check_box(self, row, col, number):
+            """Check if number can be placed in 3x3 box"""
             start_row = (row // 3) * 3
             start_col = (col // 3) * 3
             for i in range(3):
@@ -24,4 +29,5 @@ class Sodoku():
             return True
         
         def is_valid(self,row, col, number):
+            """Validate if number can be placed at given position"""
             return (self.check_row(row, number) and  self.check_column(col, number) and self.check_box(row, col, number))
